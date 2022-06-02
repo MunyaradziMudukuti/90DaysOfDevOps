7fe9fe478b01e24b2a06d92dd7dc9882691f7468
Munyaradzi Mudukuti <munyaly@gmail.com>
2022-06-02 13:03:45 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { catchError, Observable, of, startWith } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { DataState } from './enum/data.state.enum';
+import { AppState } from './interface/app.state';
+import { CustomResponse } from './interface/custom.response';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'server-management-frontend';
+export class AppComponent implements OnInit {
+
+appState$: Observable<AppState<CustomResponse>>;
+
+  constructor(private serverService: ServerService){}
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$
+    .pipe(
+      map(response => {
+     return {
+       dataState: DataState.LOADED_STATE,
+       appData: response
+      }
+  })),
+
+  startWith({dataState: DataState.LOADING_STATE}),
+  catchError((error: string) => {
+    return of({datState: DataState.ERROR_STATE, error})
+
+  });
+
+  }
 }
